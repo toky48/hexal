6c65298adeb895937959cc1897605f03438be525
Mateusz Podlasin <Podlas29@gmail.com>
2015-12-16 11:43:40 +0100

    nav animation on scroll

--- js/main.js
+++ js/main.js
@@ -14,6 +14,29 @@ $(function() {
 
     $("nav").css("transform", "none");
 
+    var navFixed = false;
+
+    $(window).on("scroll", function() {
+        var scrollPos = $(window).scrollTop()
+        var nav = $("nav > *");
+
+        if(scrollPos > arrHeight && !navFixed) {
+            nav.css("top", "-" + (arrHeight * 1.5) + "px");
+            nav.css("position", "fixed");
+            nav.animate({
+                top: "0px"
+            }, 800);
+
+            navFixed = true;
+        }
+
+        if(scrollPos === 0) {
+            nav.css("position", "absolute");
+            navFixed = false;
+        }
+    });
+
+
     adjustMenu();
     $(window).on("resize", adjustMenu);
 
