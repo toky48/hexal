4ab4ac75473038aa2ec011ba9e6fc05e5bf443eb
Mateusz Podlasin <Podlas29@gmail.com>
2015-12-18 19:39:42 +0100

    simplifying main.js code with class uses

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ $(function() {
     var arrow = $(".arrow");
     var arrowAndMask = $(".arrow, .mask");
     var nav = $("nav");
-    var navEls = $("nav > *");
-    var navElsNotArrowAndMask = navEls.filter(":not(.arrow):not(.mask)");
     var sections = $("section");
     var navLinks = $("nav a");
     var leftNavContainer = $(".leftNavContainer");
@@ -12,9 +10,7 @@ $(function() {
     var navHeight = nav.height();
     var arrHeight = $(".arrow").height();
 
-    var menuVisible = false;
-    var navFixed = false;
-    var mobileMenu;
+    var mobileMode;
 
     setupRegularOrMobileScrolling();
     $(window).on("resize", setupRegularOrMobileScrollingOnResize);
@@ -22,9 +18,11 @@ $(function() {
     adjustMenu();
     $(window).on("resize", adjustMenu);
 
-    nav.css("transform", "none");
+    $(window).on("scroll", actualizeActiveLinkInMenu);
 
-    $(window).on("scroll", function() {
+    navLinks.on("click", smoothScroll);
+
+    function actualizeActiveLinkInMenu() {
         var scrollPos = $(window).scrollTop();
 
         sections.each(function(i, section) {
@@ -38,24 +36,8 @@ $(function() {
                 }
             }
         });
-    });
+    }
 
-    navLinks.on("click", function(event) {
-        event.preventDefault();
-        var linkTarget = $(this).attr("href");
-
-        var targetPos = $(linkTarget).position();
-        if(targetPos !== undefined) {
-            $("html, body").animate({
-                scrollTop: targetPos.top - arrHeight + 1
-            }, 800);
-
-            if(mobileMenu) {
-                showOrHideMobileMenu();
-            }
-
-        }
-    });
 
     function adjustMenu() {
         var arrWidth = arrow.width();
@@ -77,6 +59,10 @@ $(function() {
     }
 
     function setupRegularScrolling() {
+        nav.removeClass("visibleMenu");
+        nav.attr("style", "");
+        arrowAndMask.attr("style", "");
+
         arrowAndMask.off("click", showOrHideMobileMenu);
         $(window).off("scroll", scrollMobileMenu);
         $(window).on("scroll", scrollRegularMenu);
@@ -87,117 +73,79 @@ $(function() {
 
         if(screenWidth < 520) {
             setupMobileScrolling();
-            mobileMenu = true;
+            mobileMode = true;
         } else if(screenWidth >= 520) {
             setupRegularScrolling();
-            mobileMenu = false;
+            mobileMode = false;
         }
     }
 
     function setupRegularOrMobileScrollingOnResize() {
         var screenWidth = window.outerWidth;
 
-        if(screenWidth < 520 && !mobileMenu) {
-            clearNavStyle();
+        if(screenWidth < 520 && !mobileMode) {
             setupMobileScrolling();
-            fakeMobileScroll();
 
-            mobileMenu = true;
-        } else if(screenWidth >= 520 && mobileMenu) {
-            clearNavStyle();
+            mobileMode = true;
+        } else if(screenWidth >= 520 && mobileMode) {
             setupRegularScrolling();
-            fakeRegularScroll();
 
-            mobileMenu = false;
-            menuVisible = false;
+            mobileMode = false;
         }
-    }
-
-    function clearNavStyle() {
-        nav.attr("style", "transform: none;");
-        navEls.attr("style", "");
     }
 
     function showOrHideMobileMenu() {
         var navHeight = nav.height();
-        if(menuVisible) {
-            nav.animate({"top": - navHeight - arrHeight / 2}, 800);
-            var scrollPos = $(window).scrollTop();
+        var scrollPos = $(window).scrollTop();
+
+        if(nav.hasClass("visibleMenu")) {
+            nav.removeClass("visibleMenu");
             if(scrollPos === 0) {
-                nav.css("position", "absolute");
+                nav.removeClass("fixedMenu");
             }
-
-            menuVisible = false;
         } else {
-            nav.animate({"top": "0px"}, 800);
-            nav.css("position", "fixed");
-
-            menuVisible = true;
+            nav.addClass("visibleMenu");
         }
     }
 
     function scrollMobileMenu() {
         var scrollPos = $(window).scrollTop();
-        var navHeight = nav.height();
 
-        if(scrollPos > arrHeight && !navFixed && !menuVisible) {
-            nav.css("top", - navHeight - arrHeight * 2);
-            nav.css("position", "fixed");
-            nav.animate({"top": - navHeight - arrHeight / 2}, 800);
-
-            navFixed = true;
+        if(scrollPos > arrHeight * 1.5 && !nav.hasClass("fixedMenu")) {
+            nav.addClass("fixedMenu");
         }
 
         if(scrollPos === 0) {
-            nav.css("position", "absolute");
-
-            navFixed = false;
-        }
-    }
-
-    function fakeMobileScroll() {
-        var navHeight = nav.height();
-        var scrollPos = $(window).scrollTop();
-
-        if(scrollPos !== 0) {
-            nav.css("position", "fixed");
-            nav.css("top", - navHeight - arrHeight / 2);
-
-            navFixed = true;
+            nav.removeClass("fixedMenu");
         }
     }
 
     function scrollRegularMenu() {
         var scrollPos = $(window).scrollTop();
 
-        if(scrollPos > arrHeight && !navFixed) {
-            navEls.css("top", "-" + (arrHeight * 1.5) + "px");
-            navEls.css("position", "fixed");
-            navElsNotArrowAndMask.css("border-bottom-style", "none");
-            navEls.animate({top: "0px"}, 800);
-
-            navFixed = true;
+        if(scrollPos > arrHeight * 1.5 && !nav.hasClass("fixedMenu")) {
+            nav.addClass("fixedMenu");
         }
 
         if(scrollPos === 0) {
-            navEls.each(function(_, el) {
-                el.style.removeProperty("position");
-                el.style.removeProperty("border-bottom-style");
-            });
-
-            navFixed = false;
+            nav.removeClass("fixedMenu");
         }
     }
 
-    function fakeRegularScroll() {
-        var scrollPos = $(window).scrollTop();
+    function smoothScroll(event) {
+        event.preventDefault();
+        var linkTarget = $(this).attr("href");
 
-        if(scrollPos !== 0) {
-            navEls.css("position", "fixed");
-            navElsNotArrowAndMask.css("border-bottom-style", "none");
-            navEls.css("top", "0px");
+        var targetPos = $(linkTarget).position();
+        if(targetPos !== undefined) {
+            $("html, body").animate({
+                scrollTop: targetPos.top - arrHeight + 1
+            }, 800);
 
-            navFixed = true;
+            if(mobileMode) {
+                showOrHideMobileMenu();
+            }
+
         }
     }
 });
